516606bc14cbf1d9b5d08502e7df7034111c0cce
Max Blaushild <blaushmild@gmail.com>
2015-03-12 13:04:12 -0400

    implements backbone, routes correctly for home

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,36 @@
-var menu = (function (module) {
+'use strict';
 
-  var host = "http://localhost:3000";
+var router = (function (module) {
 
+  module.host = "http://localhost:3000";
 
+  var Router = Backbone.Router.extend({
+  routes: {
+    '':'home',
+    'home': 'home'
+  },
+  home: function(){
 
+    $('#container').empty();
+    $.ajax({
+      url: module.host + "/products",
+      type: 'GET'
+    }).done(function(data){
+      console.log(data);
+    }).fail();
+  },
+});
+
+module.router = new Router();
+
+module.backbone = function(){
+  Backbone.history.start();
+};
   return module;
-
-})(menu || {});
+})(router || {});
 
 
 
 $(document).ready(function(){
-  menu.init();
+  router.backbone();
 });
