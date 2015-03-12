53eec8b2b190f2d0c64c9bf5dec4214f7ee24d73
Max Blaushild <blaushmild@gmail.com>
2015-03-12 15:35:34 -0400

    adds stripe link to our index, creates a basic payment form partial, creates a payment route, we bagin implementing the credit card processing front end methods. we can return auth tokens from strip api

---
+++ app/js/payment.js
@@ -0,0 +1,48 @@
+var payment = (function (module) {
+
+  Stripe.setPublishableKey('pk_test_H4OZUG84CRFTuQ0ffA8SQg6g');
+
+  module.pay = function(){
+  Stripe.card.createToken({
+    number: $('#number').val(),
+    cvc: $('#cvc').val(),
+    exp_month: $('#exp-month').val(),
+    exp_year: $('#exp-year').val()
+  }, stripeResponseHandler);
+};
+
+  stripeResponseHandler = function(status, response) {
+    console.log(response);
+    var $form = $('#payment-form');
+
+    if (response.error) {
+      // Show the errors on the form
+      $form.find('.payment-errors').text(response.error.message);
+      $form.find('button').prop('disabled', false);
+    } else {
+      // response contains id and card, which contains additional card details
+      var token = response.id;
+      // Insert the token into the form so it gets submitted to the server
+      $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+      // and submit
+      $form.get(0).submit();
+    }
+  };
+
+
+
+  module.init = function(){
+    console.log('im the payment')
+    $('#content').on('click', 'button', function(){
+      event.preventDefault();
+      payment.pay();
+    });
+  };
+
+  return module;
+
+})(payment || {});
+
+$(document).ready(function(){
+  payment.init();
+});
--- app/js/router.js
+++ app/js/router.js
@@ -5,26 +5,30 @@ var router = (function (module) {
   module.host = "http://localhost:3000";
 
   var Router = Backbone.Router.extend({
-  routes: {
-    '':'home',
-    'home': 'home'
-  },
-  home: function(){
+    routes: {
+      '':'home',
+      'home': 'home',
+      'payments': 'payments'
+    },
+    home: function(){
+      $('#content').empty();
+      $.ajax({
+        url: module.host + "/products",
+        type: 'GET'
+      }).done(menu.renderMenu).fail();
+    },
+    payments: function(){
+      $('#content').empty().load('partials/payment-form.html');
+    },
+  });
 
-    $('#container').empty();
-    $.ajax({
-      url: module.host + "/products",
-      type: 'GET'
-    }).done(menu.renderMenu).fail();
-  },
-});
+  module.router = new Router();
 
-module.router = new Router();
+  module.backbone = function(){
+    Backbone.history.start();
+  };
+    return module;
 
-module.backbone = function(){
-  Backbone.history.start();
-};
-  return module;
 })(router || {});
 
 
