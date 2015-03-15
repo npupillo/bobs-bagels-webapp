a1a59c6fd5dcdcbb4defd7f02bf86035a6733e07
ironman <ironman@Brandons-MacBook-Pro.local>
2015-03-15 10:41:46 -0400

    created javascript_test.html and js/test.js to test object creation

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,68 @@
-/* global describe, it */
+describe('Router', function () {
+     it("provides the 'router' object", function () {
+       // Expect exists and is an object.
+       expect(router).to.be.an("object");
 
-(function () {
-  'use strict';
+});
+     });
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
+     describe('Menu', function () {
+     it("provides the 'menu' object", function () {
+       // Expect exists and is an object.
+       expect(menu).to.be.an("object");
 
-      });
-    });
-  });
-})();
+});
+});
+
+     describe('Payment', function () {
+     it("provides the 'payment' object", function () {
+       // Expect exists and is an object.
+       expect(payment).to.be.an("object");
+
+});
+});
+
+     describe('Cart', function () {
+     it("provides the 'cart' object", function () {
+       // Expect exists and is an object.
+       expect(cart).to.be.an("object");
+
+});
+});
+
+ describe('Delivery', function () {
+     it("provides the 'delivery' object", function () {
+       // Expect exists and is an object.
+       expect(delivery).to.be.an("object");
+
+});
+});
+
+
+
+
+
+
+// describe("Test timing", function () {
+//      it("should be a fast test", function (done) {
+//        expect("hi").to.equal("hi");
+// done(); });
+//      it("should be a medium test", function (done) {
+//  setTimeout(function () {
+//       expect("hi").to.equal("hi");
+//       done();
+// }, 40); });
+//   it("should be a slow test", function (done) {
+//     setTimeout(function () {
+//       expect("hi").to.equal("hi");
+//       done();
+//     }, 100);
+// });
+//   it("should be a timeout failure", function (done) {
+//     setTimeout(function () {
+//       expect("hi").to.equal("hi");
+//       done();
+//     }, 2001);
+// });
+
+// });
