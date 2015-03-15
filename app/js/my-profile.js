c43359f3720f13e8bab913b58ab78824612992ae
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-03-15 16:48:13 -0400

    PATCH for updating my-profile still not working

--- app/js/my-profile.js
+++ app/js/my-profile.js
@@ -1,68 +1,76 @@
+var profile = (function (module){
 
-var source = $("#user_profile_template").html();
-var template = Handlebars.compile(source);
+  module.handleForm = function(user_id){
+      console.log('inside handleForm, user id is: ' + user_id);
 
-//hard coded mock JSON DATA for handlebars template:
-var user_data =
-{
-  users: [
-    {
-      "id": 1,
-      "first_name": "Joey",
-      "last_name": "Schmoe",
-      "email": "joe@schmoe.com",
-      "phone_number": "555-555-5555",
-      "address_1": "502 Main St",
-      "address_2": "Apt 2",
-      "address_zipcode": "02103"
-    },
-  ]
-};
-$("#display_user").html(template(user_data));
 
-  //hide EDIT PROFILE FORM on initial load
-  $('#edit-profile-form').hide();
+      $('form#edit-profile-form').on('submit',function(event, user_id){
+        console.log('inside Save Profile button, user id is: ' + user_id)
 
-  //get id of user when EDIT PROFILE button is clicked
-  $(".edit-profile").on('click', function(event){
-
-  //toggle show/hide
-   $('#edit-profile-form').toggle();
-
+        event.preventDefault();
+        console.log('SAVE PROFILE button clicked');
 // debugger;
-    //get the id of this user when Edit Profile button is clicked
-    var user_id = $(this).attr("id").replace('edit-profile-b-', '');
-
-    console.log("EDIT PROFILE clicked, user id is: " + user_id);
-
-    //call editReview, pass event and movie_id
-    // MovieApp.editReview(event, movie_id);
-
-  });
-
-
-
-  $('form#edit-profile-form').on('submit',function(event){
-    event.preventDefault();
-    console.log('SAVE PROFILE button clicked');
-
-    $.ajax({
-      url: 'http://localhost:3000/users/' + user_id,
-      type: 'PATCH',
-      data: {
-        user: {
-          first_name: $('#user-first-name-field-' + user_id).val(),
-          last_name: $('#user-last-name-field-' + user_id).val(),
-          email: $('#user-email-field-' + user_id).val(),
-          phone_number: $('#user-phone-field-' + user_id).val(),
-          address_1: $('#user-address-1-field-' + user_id).val(),
-          address_2: $('#user-address-2-field-' + user_id).val(),
-          address_zipcode: $('#user-zipcode-field-' + user_id).val()
+        $.ajax({
+          url: 'http://localhost:3000/users/1',
+          type: 'PATCH',
+          dataType: 'JSON',
+          data:
+          { user: {
+              first_name: $('#user-first-name-field').val(),
+              last_name: $('#user-last-name-field').val(),
+              email: $('#user-email-field').val(),
+              phone_number: $('#user-phone-field').val(),
+              address_1: $('#user-address-1-field').val(),
+              address_2: $('#user-address-2-field').val(),
+              address_zipcode: $('#user-zipcode-field').val()
+          }
         }
-      }
-    }).done(function(data){
+
+        }).done(function(data){
+          console.log(data);
+        }).fail(function(jqXHR, textStatus, errorThrown){
+          console.log(jqXHR, textStatus, errorThrown);
+        });
+      }); // end form#edit-profile-form
+  }; // end module.showForm
+
+
+ module.init = function(){
+    $.ajax({
+    url: 'http://localhost:3000/users/1',
+    type: 'GET',
+    dataType: 'JSON'
+    })
+    .done(function(data) {
       console.log(data);
-    }).fail(function(jqXHR, textStatus, errorThrown){
-      console.log(jqXHR, textStatus, errorThrown);
-    });
-  }); // end form#edit-profile-form
+      var template = Handlebars.compile($('#user_profile_template').html());
+      $("#content").html(template({
+          user: data //send user data to handlebars template
+        }));
+
+      //hide EDIT PROFILE FORM on initial load
+      $('#edit-profile-form').hide();
+
+      //get id of user when EDIT PROFILE button is clicked
+      $(".edit-profile").on('click', function(event){
+
+      //get the id of this user when Edit Profile button is clicked
+      var user_id = $(this).attr("id").replace('edit-profile-b-', '');
+      console.log("EDIT PROFILE clicked, user id is: " + user_id);
+
+  // debugger;
+        $('#edit-profile-form').toggle();
+
+        profile.handleForm(user_id);
+
+        });
+      }).fail(function(jqXHR, textStatus, errorThrow) {
+        console.log(jqXHR, textStatus, errorThrow);
+      });
+
+    }; //end module.init
+
+
+return module;
+
+})(profile || {});
