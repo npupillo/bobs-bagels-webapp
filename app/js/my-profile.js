d6349b8afe1a11df05fd5b4cf61946e37a29342e
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-03-15 12:54:24 -0400

    Edit Profile button toggles Edit Profile Form

--- app/js/my-profile.js
+++ app/js/my-profile.js
@@ -1,13 +1,14 @@
+
 var source = $("#user_profile_template").html();
 var template = Handlebars.compile(source);
 
-//hard coded mock json data for handlebars template:
+//hard coded mock JSON DATA for handlebars template:
 var user_data =
 {
   users: [
     {
       "id": 1,
-      "first_name": "Joe",
+      "first_name": "Joey",
       "last_name": "Schmoe",
       "email": "joe@schmoe.com",
       "phone_number": "555-555-5555",
@@ -19,29 +20,49 @@ var user_data =
 };
 $("#display_user").html(template(user_data));
 
+  //hide EDIT PROFILE FORM on initial load
+  $('#edit-profile-form').hide();
+
+  //get id of user when EDIT PROFILE button is clicked
+  $(".edit-profile").on('click', function(event){
+
+  //toggle show/hide
+   $('#edit-profile-form').toggle();
+
+// debugger;
+    //get the id of this user when Edit Profile button is clicked
+    var user_id = $(this).attr("id").replace('edit-profile-b-', '');
+
+    console.log("EDIT PROFILE clicked, user id is: " + user_id);
+
+    //call editReview, pass event and movie_id
+    // MovieApp.editReview(event, movie_id);
+
+  });
+
+
 
   $('form#edit-profile-form').on('submit',function(event){
     event.preventDefault();
+    console.log('SAVE PROFILE button clicked');
 
-    alert('BUTTON CLICKED');
-
-  // $.ajax({
-  //   url: 'http://localhost:3000/users/' + the_user_id,
-  //   type: 'PATCH',
-  //   dataType: 'JSON',
-  //   data: {
-  //     review: {
-  //       comment: $('#review-comment-field-' + review_id).val(),
-  //       // star_rating: Number($('.rating-input:checked')[0].value),
-  //       // reviewer_name: $('#review-reviewer-name').val()
-  //     }
-  //   }
-  // }).done(function(data){
-  //   console.log(data)
-  //   // !!!!  after submitting the updated user data, load the new user data to #user_profile_template
-
-  // }).fail();
-
-
+    $.ajax({
+      url: 'http://localhost:3000/users/' + user_id,
+      type: 'PATCH',
+      data: {
+        user: {
+          first_name: $('#user-first-name-field-' + user_id).val(),
+          last_name: $('#user-last-name-field-' + user_id).val(),
+          email: $('#user-email-field-' + user_id).val(),
+          phone_number: $('#user-phone-field-' + user_id).val(),
+          address_1: $('#user-address-1-field-' + user_id).val(),
+          address_2: $('#user-address-2-field-' + user_id).val(),
+          address_zipcode: $('#user-zipcode-field-' + user_id).val()
+        }
+      }
+    }).done(function(data){
+      console.log(data);
+    }).fail(function(jqXHR, textStatus, errorThrown){
+      console.log(jqXHR, textStatus, errorThrown);
+    });
   }); // end form#edit-profile-form
-
