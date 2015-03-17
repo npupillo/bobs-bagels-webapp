b269b67a91cf3e6d0626153c2b256ce5790df5be
Max Blaushild <blaushmild@gmail.com>
2015-03-17 16:01:12 -0400

    corresponding front end changes to back end charge calculations

--- app/js/payment.js
+++ app/js/payment.js
@@ -9,7 +9,6 @@ var payment = (function (module) {
   };
 
 	module.card_pay = function () {
-					debugger;
 		Stripe.card.createToken({
 			number: $('#number').val(),
 			cvc: $('#cvc').val(),
@@ -19,48 +18,26 @@ var payment = (function (module) {
 	};
 
 	_stripeResponseHandler = function (status, response) {
-					debugger;
 		console.log(response);
 
 			var $form = $('#payment-form');
-			var $checkStatus = function () {
-				if ($('input[name="store-payment-info"]:checked').attr('checked') == 'checked'){
-				return true;
-				} else {
-					return false;
-				}
-			}
 
 		if (response.error) {
 			$form.find('.payment-errors').text(response.error.message);
 			$form.find('button').prop('disabled', false);
 		} else {
-			debugger;
 			var token = response.id;
 			order.submitOrder(token);
 		};
 	};
 
-	_makePayment = function(token){
-		debugger;
-		$.ajax({
-		url: 'http://localhost:3000/charges/make_charge',
-		type: 'POST',
-		data: {
-			charge: {
-				token: token,
-				cart: localStorage['cart'],
-				store_info: $checkStatus
-					//			  customer_id: "cus_5rOOrVnSCnMESX" // going to have to expand this to two functions that make charges by user id or by card token.
-				}
+	module.checkStatus = function () {
+		if ($('input[name="store-payment-info"]:checked').attr('checked') == 'checked'){
+			return true;
+		} else {
+			return false;
 			}
-		}).done(function (data) {
-			console.log(data);
-			order.submitOrder();
-		}).fail(function (jqXHR, textStatus, errorThrown) {
-			console.log(jqXHR, textStatus, errorThrown);
-		});
-	};
+		};
 
 return module;
 
